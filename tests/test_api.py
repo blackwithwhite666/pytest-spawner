a3faad0e3b09aa2a85147b81b1ed1522bbd0da41
Lipin Dmitriy <dldmitry@yandex-team.ru>
2015-07-31 00:35:38 +0300

    add some api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,22 @@
 # coding: utf-8
 
+import pytest
+
+from pytest_spawner.error import ProcessError
+
 
 def test_check_output(spawner):
     assert spawner.check_output('echo test').strip() == b'test'
+    with pytest.raises(ProcessError):
+        spawner.check_output('sh -c "exit 1"')
 
 
 def test_check_call(spawner):
     assert spawner.check_call('sh -c "exit 0"') == 0
+    with pytest.raises(ProcessError):
+        spawner.check_call('sh -c "exit 1"')
+
+
+def test_spawn(spawner):
+    with spawner.spawn("bash", "bash -i"):
+        pass
