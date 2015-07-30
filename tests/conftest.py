e6bfa0555d7c80925af215b372228882dc7907a1
Lipin Dmitriy <dldmitry@yandex-team.ru>
2015-07-31 00:30:56 +0300

    fix tests running from tox

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,7 @@
 # coding: utf-8
+
+# register plugin the last to properly compute coverage
+from pytest_spawner.plugin import (
+    pytest_configure,
+    spawner
+)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
-def test_check_output(spawner):
-    assert spawner.check_output('echo test', timeout=5).strip() == b'test'
 
+def test_check_output(spawner):
+    assert spawner.check_output('echo test').strip() == b'test'
+
+
+def test_check_call(spawner):
+    assert spawner.check_call('sh -c "exit 0"') == 0
